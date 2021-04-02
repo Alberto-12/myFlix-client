29ce33a9742fe21421d2404ed1d6a9cef95a2140
Alberto Serafini <ilgumo@gmai.com>
2021-04-02 13:27:40 -0500

    adding README file

--- dist/src.78399e21.js
+++ dist/src.78399e21.js
@@ -29727,7 +29727,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62022" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49611" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
