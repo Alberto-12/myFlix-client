411fa78d512248910a809ea71908f91779996cf1
Alberto Serafini <ilgumo@gmai.com>
2021-04-21 13:34:38 -0500

    corse-propType

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -27,3 +27,10 @@ export function LoginView(props) {
     </form>
   );
 }
+
+LoginView.propTypes = {
+    username: PropTypes.shape({
+    username: PropTypes.string
+    }).isRequired,
+    
+  };
