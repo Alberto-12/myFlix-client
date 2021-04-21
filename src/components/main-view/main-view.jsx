472f94b9247ad744aa19bb6651bfa2cabd1a6a5b
Alberto Serafini <ilgumo@gmai.com>
2021-04-21 14:04:36 -0500

    reactBoostrap

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -4,6 +4,7 @@ import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 import { LoginView } from '../login-view/login-view';
 import { RegistrationView } from '../registration-view/registration-view';
+import {Row,Col} from 'react-bootstrap';
 
 export class MainView extends React.Component {
   constructor() {
@@ -48,18 +49,22 @@ export class MainView extends React.Component {
     if (!movies) return <div className="main-view"/>;
 
     return (
-        <div className="main-view">
-  
-          {/*If the state of `selectedMovie` is not null, that selected movie will be returned otherwise, all *movies will be returned*/}
-  
+        <Row className="main-view justify-content-md-center">
           {selectedMovie
-            ? <MovieView movie={selectedMovie}/>
+            ? (
+              <Col md={8}>
+                <MovieView movie={selectedMovie} onBackClick={movie => this.onMovieClick(null)} />
+              </Col>
+            )
             : movies.map(movie => (
-              <MovieCard key={movie._id} movie={movie} onClick={movie => this.onMovieClick(movie)}/>
-           ))
+              <Col md={3}>
+                <MovieCard key={movie._id} movie={movie} onClick={movie => this.onMovieClick(movie)}/>
+              </Col>
+            ))
           }
-        </div>
+        </Row>
       );
+      
     }
   
 }
