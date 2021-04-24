64cde00d6d1dd07cfa2d1013970af98de5c6d90c
Alberto Serafini <ilgumo@gmai.com>
2021-04-24 14:04:22 -0500

    react-router

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import axios from 'axios';
-import { MovieCard } from '../movie-card/movie-card';
-import { MovieView } from '../movie-view/movie-view';
-import { LoginView } from '../login-view/login-view';
-import { RegistrationView } from '../registration-view/registration-view';
-import {Row,Col} from 'react-bootstrap';
+import axios from "axios";
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view";
+import { LoginView } from "../login-view/login-view";
+import { RegistrationView } from "../registration-view/registration-view";
+import { Row, Col } from "react-bootstrap";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
 export class MainView extends React.Component {
   constructor() {
@@ -12,59 +13,135 @@ export class MainView extends React.Component {
     this.state = {
       movies: [],
       selectedMovie: null,
-      user: null
+      user: null,
     };
   }
 
-  componentDidMount(){
-    axios.get('https://austin-night.herokuapp.com/movies')
-      .then(response => {
+  componentDidMount() {
+    let accessToken = localStorage.getItem("token");
+    if (accessToken !== null) {
+      this.setState({
+        user: localStorage.getItem("user"),
+      });
+      this.getMovies(accessToken);
+    }
+  }
+
+  onLoggedIn(authData) {
+    console.log(authData);
+    this.setState({
+      user: authData.user.Username,
+    });
+
+    localStorage.setItem("token", authData.token);
+    localStorage.setItem("user", authData.user.Username);
+    this.getMovies(authData.token);
+  }
+
+  onLoggedOut() {
+    localStorage.removeItem("token");
+    localStorage.removeItem("user");
+    this.setState({
+      user: null,
+    });
+  }
+
+  setSelectedMovie(newSelectedMovie) {
+    this.setState({
+      selectedMovie: newSelectedMovie,
+    });
+  }
+
+  getMovies(token) {
+    axios
+      .get("https://austin-night.herokuapp.com/movies", {
+        headers: { Authorization: `Bearer ${token}` },
+      })
+      .then((response) => {
+        // Assign the result to the state
         this.setState({
-          movies: response.data
+          movies: response.data,
         });
       })
-      .catch(error => {
+      .catch(function (error) {
         console.log(error);
       });
   }
 
-  onLoggedIn(user) {
-    this.setState({
-      user
-    });
-  }
-
-
-
-  setSelectedMovie(newSelectedMovie) {
-    this.setState({
-      selectedMovie: newSelectedMovie
-    });
-  }
   render() {
-    const { movies, selectedMovie,user } = this.state;
+    const { movies, user } = this.state;
 
-    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
-
-    if (!movies) return <div className="main-view"/>;
-
-    return (
-        <Row className="main-view justify-content-md-center">
-          {selectedMovie
-            ? (
-              <Col md={8}>
-                <MovieView movie={selectedMovie} onBackClick={movie => this.onMovieClick(null)} />
-              </Col>
-            )
-            : movies.map(movie => (
-              <Col md={3}>
-                <MovieCard key={movie._id} movie={movie} onClick={movie => this.onMovieClick(movie)}/>
-              </Col>
-            ))
-          }
+    if (!user)
+      return (
+        <Row>
+          <Col>
+            <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />
+          </Col>
         </Row>
       );
-      
-    }
-  
+    if (movies.length === 0) return <div className="main-view" />;
+
+    return (
+      <Router>
+        <Row className="main-view justify-content-md-center">
+          <Route
+            exact
+            path="/"
+            render={() => {
+              return movies.map((m) => (
+                <Col md={3} key={m._id}>
+                  <MovieCard movie={m} />
+                </Col>
+              ));
+            }}
+          />
+          <Route
+            path="/movies/:movieId"
+            render={({ match }) => {
+              return (
+                <Col md={8}>
+                  <MovieView
+                    movie={movies.find((m) => m._id === match.params.movieId)}
+                  />
+                </Col>
+              );
+            }}
+          />
+
+          {/* <Route
+            path="/directors/:name"
+            render={({ match }) => {
+              if (movies.length === 0) return <div className="main-view" />;
+              return (
+                <Col md={8}>
+                  <DirectorView
+                    director={
+                      movies.find((m) => m.Director.Name === match.params.name)
+                        .Director
+                    }
+                  />
+                </Col>
+              );
+            }}
+          />
+          <Route
+            path="/genres/:name"
+            render={({ match }) => {
+              if (movies.length === 0) return <div className="main-view" />;
+              return (
+                <Col md={8}>
+                  <GenreView
+                    genre={
+                      movies.find((m) => m.Genre.Name === match.params.name)
+                        .Genre
+                    }
+                  />
+                </Col>
+              );
+            }}
+          /> */}
+        </Row>
+      </Router>
+    );
+  }
 }
--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,36 +1,38 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import Button from 'react-bootstrap/Button';
 
 export class MovieView extends React.Component {
-    componentDidMount() {
-        document.addEventListener('keypress', event => {
-          console.log(event.key);
-        });
-      }
-    
+  componentDidMount() {
+    document.addEventListener("keypress", (event) => {
+      console.log(event.key);
+    });
+  }
 
-    render() {
-        const { movie, onBackClick } = this.props;
-    
-        return (
-          <div className="movie-view">
-            <div className="movie-poster">
-              <img src={movie.ImagePath} />
-            </div>
-            <div className="movie-title">
-              <span className="label">Title: </span>
-              <span className="value">{movie.Title}</span>
-            </div>
-            <div className="movie-description">
-              <span className="label">Description: </span>
-              <span className="value">{movie.Description}</span>
-            </div>
-            <div className="movie-director">
-              <span className="label">Director: </span>
-              <span className="value">{movie.Director}</span>
-            </div>
-            <button onClick={() => { onBackClick(null); }}>Back</button>
-    
-          </div>
-        );
-      }
+  render() {
+    const { movie, onBackClick } = this.props;
+
+    return (
+      <div className="movie-view">
+        <div className="movie-poster">
+          <img src={movie.ImagePath} />
+        </div>
+        <div className="movie-title">
+          <span className="label">Title: </span>
+          <span className="value">{movie.Title}</span>
+        </div>
+        <div className="movie-description">
+          <span className="label">Description: </span>
+          <span className="value">{movie.Description}</span>
+        </div>
+        <Link to={`/directors/${movie.Director.Name}`}>
+          <Button variant="link">Director</Button>
+        </Link>
+
+        <Link to={`/genres/${movie.Genre.Name}`}>
+          <Button variant="link">Genre</Button>
+        </Link>
+      </div>
+    );
+  }
 }
